1240fc5d82ed65d4417957c6a77b7cf5a0a429f7
Iwo Strzeboński <iwo.strzebonski@gmail.com>
2021-04-28 11:09:23 +0200

    Drobne zmiany

--- main.py
+++ main.py
@@ -13,37 +13,30 @@ client = commands.Bot(command_prefix=PREFIX)
 @client.event
 async def on_ready() :
     await client.change_presence(
-        status=discord.Status.active,
+        status=discord.Status.online,
         activity=discord.Activity(
             type=discord.ActivityType.listening,
             name=PREFIX+'help'
         )
     )
     print('I am online')
+    print(token)
+    stdout.flush()
 
 
 @client.command()
-async def ping(ctx) :
+async def ping(ctx):
     await ctx.send(f'🏓 Pong with {str(round(client.latency, 2))}')
 
 
 @client.command(name='whoami')
-async def whoami(ctx) :
-    await ctx.send(f'You are {ctx.message.author.name}, {ctx.message.author}')
+async def whoami(ctx):
+    await ctx.send(f'You are {ctx.message.author.mention}')
 
 
 @client.command()
-async def clear(ctx, amount=3) :
+async def clear(ctx, amount=4):
     await ctx.channel.purge(limit=amount)
 
 
-@client.command()
-async def kill_bot(ctx):
-    await ctx.send('F in chat for meself :(')
-    exit()
-
-
 client.run(token)
-
-print(token)
-stdout.flush()
